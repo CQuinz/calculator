4ec8a324a958a219c1693ece9c04b9b45d062a4b
Christian <christian.quinlan36@gmail.com>
2021-03-25 12:55:22 +0000

    Adding event listners for numbers and operators. displaying those numbers on the calculator display

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
+
+const sendNumValue = (number)=>{
+  // If current dispaly value is 0, weant to replace it, if not, add number
+  const displayValue = calculatorDisplay.textContent;
+  // Tierny condition
+  calculatorDisplay.textContent = displayValue === '0' ? number: displayValue + number;
+}
+
+// Add Event Listeners for numbers, operators, and decimal buttons
+inputBtns.forEach((inputBtn)=>{
+  if(inputBtn.classList.length === 0){
+    inputBtn.addEventListener('click', ()=> sendNumValue(inputBtn.value));
+  }else if(inputBtn.classList.contains('operator')){
+    inputBtn.addEventListener('click', ()=> sendNumValue(inputBtn.value));
+  }else if(inputBtn.classList.contains('decimal')){
+    inputBtn.addEventListener('click', ()=> sendNumValue(inputBtn.value));
+  }
+});
