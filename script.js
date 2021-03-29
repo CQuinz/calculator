d40b7b8c8dbdacd90f981610c32c6a1465f8fbe1
Christian <christian.quinlan36@gmail.com>
2021-03-29 13:16:09 +0100

    changed eventlistener on operator buttons to run a new function to capture the operater value input plus the first value input and store them in coresponding varaibles

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 const sendNumValue = (number)=>{
   // If current dispaly value is 0, weant to replace it, if not, add number
   const displayValue = calculatorDisplay.textContent;
@@ -16,12 +20,23 @@ const addDecimal = ()=>{
   }
 }
 
+const useOperator = (operator)=>{
+  const currentValue = Number(calculatorDisplay.textContent);
+  // Assign firstValue if has no value
+  if(!firstValue){
+    firstValue = currentValue;
+  }
+  operatorValue = operator;
+  console.log('firstValue: ', firstValue);
+  console.log('operatorValue: ', operatorValue);
+}
+
 // Add Event Listeners for numbers, operators, and decimal buttons
 inputBtns.forEach((inputBtn)=>{
   if(inputBtn.classList.length === 0){
     inputBtn.addEventListener('click', ()=> sendNumValue(inputBtn.value));
   }else if(inputBtn.classList.contains('operator')){
-    inputBtn.addEventListener('click', ()=> sendNumValue(inputBtn.value));
+    inputBtn.addEventListener('click', ()=> useOperator(inputBtn.value));
   }else if(inputBtn.classList.contains('decimal')){
     inputBtn.addEventListener('click', addDecimal);
   }
